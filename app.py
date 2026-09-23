973d89578c026ba8c76a57660f27ad8fda8a96fd
ALM1348 <ariane.landu1@gmail.com>
2026-09-23 21:07:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
-'''
+
 figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
 
 figure.write_html('ventes-par-region.html')
 
 print('ventes-par-région.html généré avec succès !')
-'''
+
 figure = px.pie(données, values='qte', names='produit', title='les ventes par produit,')
 figure.write_html('ventes-par-produit.html')
 print('ventes-par-produit.html généré avec succès !')
